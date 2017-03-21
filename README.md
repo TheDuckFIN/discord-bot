1b18d6d35abca7661462285ca0e6054710af90ab
TheDuckFIN <valtteri.lakaniemi@gmail.com>
2017-03-22 00:09:50 +0200

    the bot can now join channels

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # discord-bot
+
+## How to install
+1. clone this repository
+2. set up your secret tokens by filling the correct info to `secret_config_example.json` and removing `_example` from the filename (or copying, idc)
+3. run the program (currently `node index.js`)
+
+## Requirements
+* ffmpeg ( `sudo apt-get install ffmpeg` on ubuntu 16.04 )
---
+++ src/commands/join.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const logger = require(__appBase + '/src/logger');
+
+function handler(client, message, params) {
+  let channel = message.member.voiceChannel;
+
+  if (!channel) {
+    logger.debug('Join command called, but can\'t join because the user is not on any voice channel.');
+  }else {
+    channel.join()
+      .then(connection => logger.debug('Joined voice channel ' + channel.name))
+      .catch(logger.error)
+  }
+}
+
+module.exports = (commands_list) => {
+  commands_list['join'] = handler;
+}
